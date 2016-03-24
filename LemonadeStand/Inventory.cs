ec495da54e8934596450fb6ca7c98fca8202270a
huy <htngo23@gmail.com>
2016-03-24 11:17:40 -0500

    first commit

---
+++ LemonadeStand/Inventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class Inventory
+    {
+       public double cost;
+       public int quantity;
+
+        public item(double cost, int quantity);
+    }
+}
